6b4228e032cabbc63f28552a90aa9d2acc63720c
alexey <araga@arbor.net>
2015-02-19 21:07:02 +1100

    Worker to know its master!

--- spark-worker/bootstrap.sh
+++ spark-worker/bootstrap.sh
@@ -3,7 +3,7 @@
 #service sshd stop
 #/usr/sbin/sshd -D
 
-$SPARK_HOME/sbin/start-slave.sh
+$SPARK_HOME/sbin/start-slave.sh -h spark://$SPARK_MASTER_IP:7077
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
