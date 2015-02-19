aae6e8399f99b6431d42d1de3a6abd8fd79499cf
alexey <araga@arbor.net>
2015-02-19 20:19:58 +1100

    bootstraping

---
+++ spark-master/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#service sshd stop
+#/usr/sbin/sshd -D
+
+$SPARK_HOME/sbin/start-master.sh
+
+if [[ $1 == "-d" ]]; then
+  while true; do sleep 1000; done
+fi
+
+if [[ $1 == "-bash" ]]; then
+  /bin/bash $*
+fi
